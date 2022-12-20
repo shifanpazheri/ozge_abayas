a77fb8fca24df4e2810d46550bc5c3047832c0ea
Muhammed Shifan P <muhammedshifanpazheri@gmail.com>
2022-12-20 17:28:40 +0530

    login page with otp

--- client/src/components/LoginForm.jsx
+++ client/src/components/LoginForm.jsx
@@ -2,20 +2,56 @@
 import React from "react";
 import Button from "react-bootstrap/Button";
 import Form from "react-bootstrap/Form";
+import InputGroup from "react-bootstrap/InputGroup";
+
+function emailChosen() {
+  document.getElementById("email-input").style.display = "block";
+  document.getElementById("phone-input").style.display = "none";
+  document.getElementById("email-button").style.backgroundColor = "#9FA6B2";
+  document.getElementById("phone-button").style.backgroundColor = "#fff";
+}
+
+function phoneChosen() {
+  document.getElementById("phone-input").style.display = "block";
+  document.getElementById("email-input").style.display = "none";
+  document.getElementById("phone-button").style.backgroundColor = "#9FA6B2";
+  document.getElementById("email-button").style.backgroundColor = "#fff";
+}
 
 function BootstrapLoginForm() {
   return (
     <div className="login-page">
       <Form>
-        <h2 style={{ marginBottom: "5%" }}>Login Page</h2>
-        <Form.Group className="mb-3">
-          <Form.Control type="text" placeholder="Email or phone number" />
-        </Form.Group>
-        <Form.Group className="mb-3" controlId="formBasicPassword">
-          <Form.Control type="password" placeholder="Password" />
-        </Form.Group>
-        <Button variant="primary" type="submit">
-          Submit
+        <h2 style={{ marginBottom: "5%", color: "#9FA6B2" }}>Login Page</h2>
+        <Button
+          id="email-button"
+          variant="outline-secondary"
+          className="choose-mode"
+          onClick={emailChosen}
+        >
+          Email
+        </Button>
+        <Button
+          id="phone-button"
+          variant="outline-secondary"
+          className="choose-mode"
+          onClick={phoneChosen}
+        >
+          Phone
+        </Button>
+        <div id="email-input">
+          <Form.Group className="mb-3">
+            <Form.Control type="text" placeholder="Email" />
+          </Form.Group>
+        </div>
+        <div id="phone-input">
+          <InputGroup className="mb-3">
+            <InputGroup.Text>+91</InputGroup.Text>
+            <Form.Control placeholder="Phone Number" />
+          </InputGroup>
+        </div>
+        <Button variant="outline-secondary" type="submit">
+          Login with OTP
         </Button>
       </Form>
     </div>
